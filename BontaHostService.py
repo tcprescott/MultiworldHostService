076a2bea3ce1ab0cc1a782001a548afea0f13e6f
Chris <chris@legendserver.info>
2020-03-03 23:41:09 -0500

    Added support for Berserker's multiworld implementation
    - Rename files to indicate which implementation they host for
    - Add a service file for Berserker's implementation
    - Add a placeholder file in the data/ folder to prevent accidental exceptions from people running the server without reading the code
    - Add a couple TODO items

--- BontaHostService.py
+++ BontaHostService.py
@@ -26,12 +26,13 @@ APP = Quart(__name__)
 
 MULTIWORLDS = {}
 
+
 @APP.route('/game', methods=['POST'])
 async def create_game():
     global MULTIWORLDS
 
     data = await request.get_json()
-    
+
     if not 'multidata_url' in data and not 'token' in data:
         abort(400, description=f'Missing multidata_url or token in data')
 
@@ -80,6 +81,7 @@ async def create_game():
     )
     return response
 
+
 @APP.route('/game', methods=['GET'])
 async def get_all_games():
     global MULTIWORLDS
@@ -89,6 +91,7 @@ async def get_all_games():
         mimetype='application/json'
     )
     return response
+
 
 @APP.route('/game/<string:token>', methods=['GET'])
 async def get_game(token):
@@ -103,6 +106,7 @@ async def get_game(token):
         mimetype='application/json'
     )
     return response
+
 
 @APP.route('/game/<string:token>/msg', methods=['PUT'])
 async def update_game(token):
@@ -119,6 +123,7 @@ async def update_game(token):
     resp = await console_message(MULTIWORLDS[token]['server'], data['msg'])
     return jsonify(resp=resp, success=True)
 
+
 @APP.route('/game/<string:token>', methods=['DELETE'])
 async def delete_game(token):
     global MULTIWORLDS
@@ -129,41 +134,49 @@ async def delete_game(token):
     close_game(token)
     return jsonify(success=True)
 
+
 @APP.route('/jobs/cleanup/<int:minutes>', methods=['POST'])
 async def cleanup(minutes):
     global MULTIWORLDS
     tokens_to_clean = []
     for token in MULTIWORLDS:
-        if MULTIWORLDS[token]['date'] < datetime.datetime.now()-datetime.timedelta(minutes=minutes):
+        if MULTIWORLDS[token]['date'] < datetime.datetime.now() - datetime.timedelta(minutes=minutes):
             tokens_to_clean.append(token)
     for token in tokens_to_clean:
         close_game(token)
     return jsonify(success=True, count=len(tokens_to_clean), cleaned_tokens=tokens_to_clean)
 
+
 @APP.errorhandler(400)
 def bad_request(e):
     return jsonify(success=False, name=e.name, description=e.description, status_code=e.status_code)
+
 
 @APP.errorhandler(404)
 def game_not_found(e):
     return jsonify(success=False, name=e.name, description=e.description, status_code=e.status_code)
 
+
 @APP.errorhandler(500)
 def something_bad_happened(e):
     return jsonify(success=False, name=e.name, description=e.description, status_code=e.status_code)
+
 
 def close_game(token):
     server = MULTIWORLDS[token]['server']
     server.server.ws_server.close()
     del MULTIWORLDS[token]
 
+
 def is_port_in_use(port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         return s.connect_ex(('0.0.0.0', port)) == 0
 
+
 def random_string(length=6):
     chars = string.ascii_lowercase + string.digits
     return ''.join(random.choice(chars) for i in range(length))
+
 
 def multiworld_converter(o):
     if isinstance(o, MultiServer.Client):
@@ -189,6 +202,7 @@ def multiworld_converter(o):
         return o.__str__()
     if isinstance(o, asyncio.subprocess.Process):
         return o.pid
+
 
 async def create_multiserver(port, multidatafile):
     args = argparse.Namespace(
@@ -219,12 +233,14 @@ async def create_multiserver(port, multidatafile):
         logging.error('Failed to read multiworld data (%s)' % e)
         return
 
-    logging.info('Hosting game at %s:%d (%s)' % (ctx.host, ctx.port, 'No password' if not ctx.password else 'Password: %s' % ctx.password))
+    logging.info('Hosting game at %s:%d (%s)' % (
+    ctx.host, ctx.port, 'No password' if not ctx.password else 'Password: %s' % ctx.password))
 
     ctx.disable_save = args.disable_save
     if not ctx.disable_save:
         if not ctx.save_filename:
-            ctx.save_filename = (ctx.data_filename[:-9] if ctx.data_filename[-9:] == 'multidata' else (ctx.data_filename + '_')) + 'multisave'
+            ctx.save_filename = (ctx.data_filename[:-9] if ctx.data_filename[-9:] == 'multidata' else (
+                        ctx.data_filename + '_')) + 'multisave'
         try:
             with open(ctx.save_filename, 'rb') as f:
                 jsonobj = json.loads(zlib.decompress(f.read()).decode("utf-8"))
@@ -233,15 +249,18 @@ async def create_multiserver(port, multidatafile):
                 if not all([ctx.rom_names[tuple(rom)] == (team, slot) for rom, (team, slot) in rom_names]):
                     raise Exception('Save file mismatch, will start a new game')
                 ctx.received_items = received_items
-                logging.info('Loaded save file with %d received items for %d players' % (sum([len(p) for p in received_items.values()]), len(received_items)))
+                logging.info('Loaded save file with %d received items for %d players' % (
+                sum([len(p) for p in received_items.values()]), len(received_items)))
         except FileNotFoundError:
             logging.error('No save data found, starting a new game')
         except Exception as e:
             logging.info(e)
 
-    ctx.server = websockets.serve(functools.partial(MultiServer.server, ctx=ctx), ctx.host, ctx.port, ping_timeout=None, ping_interval=None)
+    ctx.server = websockets.serve(functools.partial(MultiServer.server, ctx=ctx), ctx.host, ctx.port, ping_timeout=None,
+                                  ping_interval=None)
     await ctx.server
     return ctx
+
 
 # this is a modified version of MultiServer.console that can accept commands
 async def console_message(ctx: MultiServer.Context, message):
@@ -291,6 +310,7 @@ async def console_message(ctx: MultiServer.Context, message):
 
     if command[0][0] != '/':
         MultiServer.notify_all(ctx, '[Server]: ' + message)
+
 
 if __name__ == '__main__':
     APP.run(host='127.0.0.1', port=5000, use_reloader=False)
