bf10725993e9add626ef658a5291571f83cca0ad
Thomas Prescott <tcprescott@gmail.com>
2020-04-19 17:02:57 -0500

    add close and msg commands

--- adminclient.py
+++ adminclient.py
@@ -10,10 +10,16 @@ if __name__ == "__main__":
     subparsers = parser.add_subparsers(dest='command')
 
     parser_update = subparsers.add_parser('update')
-
     parser_update.add_argument("token", help="server token")
     parser_update.add_argument("parameter", help="parameter to update")
     parser_update.add_argument("value", help="value to set")
+
+    parser_close = subparsers.add_parser('close')
+    parser_close.add_argument("token")
+
+    parser_msg = subparsers.add_parser('msg')
+    parser_msg.add_argument("token")
+    parser_msg.add_argument("msg")
 
     args = parser.parse_args()
     
@@ -34,5 +40,16 @@ if __name__ == "__main__":
                 'value': value
             }
         )
+    elif args.command == 'close':
+        resp = requests.delete(
+            url=f'http://localhost:5000/game/{args.token}'
+        )
+    elif args.command == 'msg':
+        resp = requests.put(
+            url=f'http://localhost:5000/game/{args.token}/msg',
+            json={
+                'msg': args.msg
+            }
+        )
 
-        print(resp.json())
+    print(resp.json())
