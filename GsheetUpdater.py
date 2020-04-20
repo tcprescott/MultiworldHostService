46048100586d6272dad3f61a0c880315d9077cb6
Thomas Prescott <tcprescott@gmail.com>
2020-04-19 20:27:02 -0500

    holy shoot it works

--- GsheetUpdater.py
+++ GsheetUpdater.py
@@ -1,11 +1,54 @@
+#!env/bin/python
+
 import argparse
-import requests
 import json
 import os
+import gspread
+from google.oauth2.service_account import Credentials
+
+import requests
 from dotenv import load_dotenv
 
 load_dotenv()
-gsheet_api_oauth = json.loads(os.environ('GSHEET_API_OAUTH'))
+
+def get_game_status(token):
+    data = []
+    resp = requests.get(
+        url=f'http://localhost:5000/game/{token}'
+    )
+    game = resp.json()
+    for slot, player_name in enumerate(game['players'][0]):
+        connection = next((item for item in game['server']['clients']['connected'] if item["slot"] == slot+1), None)
+        data.append(
+            [
+                slot+1,
+                player_name,
+                False if connection is None else True,
+                game['server']['received_items'][0].get(str(slot+1), 0),
+                game['server']['location_checks'][0].get(str(slot+1), 0),
+            ]
+        )
+    return data
+
+def update_gsheet(data, gsheetid):
+    gc = gspread.authorize(get_creds())
+    wb = gc.open_by_key(gsheetid)
+    worksheet = wb.get_worksheet(0)
+    worksheet.batch_update(
+        [
+            {
+                'range': f'A2:E{len(data)+1}',
+                'values': data
+            }
+        ]
+    )
+
+def get_creds():
+    return Credentials.from_service_account_info(
+            json.loads(os.environ['GSHEET_API_OAUTH']),
+            scopes=['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive',
+                'https://www.googleapis.com/auth/spreadsheets']
+        )
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -14,4 +57,6 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-
+    data = get_game_status(args.token)
+    print(data)
+    update_gsheet(data, args.gsheet)
