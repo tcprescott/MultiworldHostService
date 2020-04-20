f6a675f12787c6668a504abce17174852ff14892
Thomas Prescott <tcprescott@gmail.com>
2020-04-20 00:12:51 +0000

    fixes

--- adminclient.py
+++ adminclient.py
@@ -23,17 +23,16 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     
-    if args.value == 'true':
-        value = True
-    elif args.value == 'false':
-        value = False
-
-    try:
-        value = int(args.value)
-    except ValueError:
-        pass
-
     if args.command == 'update':
+        if args.value == 'true':
+            value = True
+        elif args.value == 'false':
+            value = False
+
+        try:
+            value = int(args.value)
+        except ValueError:
+            pass
         resp = requests.put(
             url=f'http://localhost:5000/game/{args.token}/{args.parameter}',
             json={
